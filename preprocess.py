00b04776b0b5d93ffe8e08d055bd960be5ef9f78
Ashley Gao <gaoye0718@gmail.com>
2021-12-06 21:48:44 -0500

    WIP

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,18 @@
 import os
-
+import shutil
 dataroot = "..//..//images//Office-31//"
 
 for domain in os.listdir(dataroot):
     for image_type in os.listdir(dataroot + domain + '//images//'):
         src = dataroot + domain + '//images//' + image_type
         dst = dataroot + domain + '//images//' + image_type + '//actual_images//'
-        os.rename(src, dst)
+        os.mkdirs(dst)
+        #os.rename(src, dst)
+        for item in os.listdir(src):
+            if os.path.isdir(item):
+                continue
+
+            original_path = src + '//' + item
+            new_path = dst + '//' + item
+            shutil.move(original_path, new_path)
+            print(new_path)
