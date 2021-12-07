a2ce473a028431445cbd57dbb089371e09dcb857
Ashley Gao <gaoye0718@gmail.com>
2021-12-06 21:40:32 -0500

    WIP

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,7 @@ import os
 dataroot = "..//..//images//Office-31//"
 
 for domain in os.listdir(dataroot):
-    for image_type in os.listdir(dataroot + domain + '\\images\\'):
-        src = dataroot + domain + '\\images\\' + image_type
-        dst = dataroot + domain+ '\\images\\' + image_type + '\\actual_images\\'
+    for image_type in os.listdir(dataroot + domain + '//images//'):
+        src = dataroot + domain + '//images//' + image_type
+        dst = dataroot + domain + '//images//' + image_type + '//actual_images//'
         os.rename(src, dst)
