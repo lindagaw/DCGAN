7cb0e4fbaff7d7561999b9933008f5f26f0051f0
Ashley Gao <gaoye0718@gmail.com>
2021-12-06 20:26:05 -0500

    WIP

--- dcgan.py
+++ dcgan.py
@@ -26,7 +26,7 @@ torch.manual_seed(manualSeed)
 
 # Root directory for dataset
 dataroot = "..//..//images//dcgan//"
-
+dataroot = "..//..//images//Office-31/images/amazon/back_pack//"
 # Number of workers for dataloader
 workers = 2
 
